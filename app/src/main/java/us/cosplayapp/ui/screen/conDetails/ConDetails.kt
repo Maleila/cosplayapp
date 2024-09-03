3db392658686e5825d9210353aa2969820d14fcc
Maleila <maleila.henrymitchell@gmail.com>
2024-09-02 20:30:24 -0500

    display basic details on con details page

--- app/src/main/java/us/cosplayapp/ui/screen/conDetails/ConDetails.kt
+++ app/src/main/java/us/cosplayapp/ui/screen/conDetails/ConDetails.kt
@@ -2,8 +2,11 @@ package us.cosplayapp.ui.screen.conDetails
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -11,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import us.cosplayapp.Con.ConWithId
 
 @Composable
@@ -67,7 +71,14 @@ fun ConDetails(con: ConWithId,
 //               onEditCosplay: (Cosplay) -> Unit = {},
 //               onAddCon: () -> Unit = {},
                conDetailsViewModel: ConDetailsViewModel) {
-    Column() {
-        Text(text = con.con.name)
+    Column(modifier = Modifier.padding(10.dp)) {
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center) {
+            Text(text = con.con.name,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(10.dp))
+        }
+        Text(text = con.con.location)
+        Text(text = con.con.dates[0] + " - " + con.con.dates[1])
     }
 }
